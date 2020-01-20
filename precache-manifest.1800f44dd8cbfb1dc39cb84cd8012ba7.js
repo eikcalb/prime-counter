458cfc72d134c83b3c6cd691399a6aa17a7154e9
Agwa Israel Onome <agwaisraelonome@gmail.com>
2020-01-20 05:08:30 +0100

    Updates

---
+++ precache-manifest.1800f44dd8cbfb1dc39cb84cd8012ba7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894e4a4cc6b35417b908c7b43a8ee6a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac8d70334489b13c2a92",
+    "url": "./static/css/main.6279f33f.chunk.css"
+  },
+  {
+    "revision": "85ff878b3acaa57a8d78",
+    "url": "./static/js/2.eefbbf7e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.eefbbf7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac8d70334489b13c2a92",
+    "url": "./static/js/main.c48e1ec5.chunk.js"
+  },
+  {
+    "revision": "24350ef29e0e32b03e33",
+    "url": "./static/js/runtime-main.6b84a0c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
